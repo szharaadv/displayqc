5a3387065d6382e3fe0b024eac381de558185eac
bernazeptian <bernazseptian@gmail.com>
2026-05-04 16:02:33 +0700

    add gitignore

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "displayqc");
+$conn = mysqli_connect("db.yadin.com", "bernaz", "", "displayqc");
 
 if (!$conn) {
     die("Koneksi database gagal: " . mysqli_connect_error());
