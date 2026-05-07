628973ff2c642298c8fc3908f1058dac0b09d47d
szharaadv <szharadiva23@gmail.com>
2026-05-07 08:55:37 +0700

    pullwaktu

--- cek_waktu.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config/koneksi.php';
-mysqli_query($conn, "SET time_zone = '+07:00'");
-
-$q = mysqli_fetch_row(mysqli_query($conn, "SELECT NOW()"));
-echo "MySQL NOW(): " . $q[0] . "<br>";
-echo "PHP date(): " . date('Y-m-d H:i:s') . "<br>";
-echo "Waktu lokal kamu: (lihat jam komputer)";
-?>
